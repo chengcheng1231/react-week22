f61864a756c0d4011955cb4d88cf4cf865d347cd
chencheng1231 <yuchenghsu1231@gmail.com>
2021-11-05 01:41:09 +0800

    week22-v8

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -38,10 +38,10 @@ function App() {
         <Router>
           <Header />
           <Switch>
-            <Route exact path="/">
+            <Route exact path="/react-week22/">
               <HomePage />
             </Route>
-            <Route path="/react-week22/about">
+            <Route path="/react-week22/">
               <AboutPage />
             </Route>
             <Route basename="/react-week22/write">
